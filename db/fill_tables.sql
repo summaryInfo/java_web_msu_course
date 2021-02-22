009eebb64dc3dbec1a729f99421f05f2eb2c94b4
Baskov Evgeniy <j-basevgser@yandex.ru>
2021-02-22 23:10:03 +0300

    Fill first out of six tables...

---
+++ db/fill_tables.sql
@@ -0,0 +1,5 @@
+INSERT INTO product_categories(category_id, name, description) VALUES
+    (1, 'продукты', NULL),
+    (2, 'бытовая химия', NULL),
+    (3, 'одежда/обувь', NULL),
+    (4, 'бытовая электроника', NULL);
