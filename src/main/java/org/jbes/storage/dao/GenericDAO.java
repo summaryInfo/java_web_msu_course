3855c7f9b9094784d2e2159243d2e1409e2ecd70
Baskov Evgeniy <j-basevgser@yandex.ru>
2021-03-21 08:01:23 +0300

    Add method to fetch all entities to generic DAO

--- src/main/java/org/jbes/storage/dao/GenericDAO.java
+++ src/main/java/org/jbes/storage/dao/GenericDAO.java
@@ -1,5 +1,10 @@
 package org.jbes.storage.dao;
 
+import java.util.List;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+import javax.persistence.TypedQuery;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 import org.jbes.storage.HibernateInitiallizationManager;
@@ -13,29 +18,41 @@ public class GenericDAO<T> {
 
     public void save(T entity) {
         Session session = HibernateInitiallizationManager.getSessionFactory().openSession();
-        Transaction tx1 = session.beginTransaction();
+        Transaction tx = session.beginTransaction();
         session.save(entity);
-        tx1.commit();
+        tx.commit();
         session.close();
     }
 
     public void update(T entity) {
         Session session = HibernateInitiallizationManager.getSessionFactory().openSession();
-        Transaction tx1 = session.beginTransaction();
+        Transaction tx = session.beginTransaction();
         session.update(entity);
-        tx1.commit();
+        tx.commit();
         session.close();
     }
 
     public void delete(T entity) {
         Session session = HibernateInitiallizationManager.getSessionFactory().openSession();
-        Transaction tx1 = session.beginTransaction();
+        Transaction tx = session.beginTransaction();
         session.delete(entity);
-        tx1.commit();
+        tx.commit();
         session.close();
     }
 
     public T findById(Long id) {
         return HibernateInitiallizationManager.getSessionFactory().openSession().get(thisClass, id);
+    }
+
+    public List<T> findAll() {
+        Session session = HibernateInitiallizationManager.getSessionFactory().openSession();
+        CriteriaBuilder cb = session.getCriteriaBuilder();
+        CriteriaQuery<T> cq = cb.createQuery(thisClass);
+        Root<T> rootEntry = cq.from(thisClass);
+        CriteriaQuery<T> all = cq.select(rootEntry);
+
+        TypedQuery<T> allQuery = session.createQuery(all);
+        session.close();
+        return allQuery.getResultList();
     }
 }
