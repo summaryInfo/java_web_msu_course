aa6ed55fc1206c64408b34ff070e412a595f8755
Baskov Evgeniy <j-basevgser@yandex.ru>
2021-03-22 04:18:39 +0300

    Move tests to the proper location

--- src/test/java/org/jbes/storage/tests/ProductCategoryDAOTest.java
+++ src/test/java/org/jbes/storage/tests/ProductCategoryDAOTest.java
@@ -12,22 +12,35 @@ public class ProductCategoryDAOTest {
         /* At this point db is initiallized with values from fill_tables.sql */
         ProductCategoryDAO dao = new ProductCategoryDAO();
         List<ProductCategory> results = dao.findAllMatching(null, null);
+        System.out.println("Got caterories:");
+        for (ProductCategory cat : results) {
+            System.out.println(cat.toString());
+        }
+        System.out.println("Expected caterories:");
         ProductCategory c1 = new ProductCategory();
         c1.setCategoryId(1L);
         c1.setName("продукты");
-        Assert.assertEquals(results.contains(c1), true, "Result does not contain all rectords");
+        c1.setDescription(null);
+        System.out.println(c1.toString());
+        Assert.assertEquals(results.contains(c1), true, "Result does not contain all rectords 1");
         ProductCategory c2 = new ProductCategory();
         c2.setCategoryId(2L);
         c2.setName("бытовая химия");
-        Assert.assertEquals(results.contains(c2), true, "Result does not contain all rectords");
+        c2.setDescription(null);
+        System.out.println(c2.toString());
+        Assert.assertEquals(results.contains(c2), true, "Result does not contain all rectords 2");
         ProductCategory c3 = new ProductCategory();
         c3.setCategoryId(3L);
         c3.setName("одежда/обувь");
-        Assert.assertEquals(results.contains(c3), true, "Result does not contain all rectords");
+        c3.setDescription(null);
+        System.out.println(c3.toString());
+        Assert.assertEquals(results.contains(c3), true, "Result does not contain all rectords 3");
         results.contains(c3);
         ProductCategory c4 = new ProductCategory();
         c4.setCategoryId(4L);
         c4.setName("бытовая электроника");
-        Assert.assertEquals(results.contains(c4), true, "Result does not contain all rectords");
+        c4.setDescription(null);
+        System.out.println(c4.toString());
+        Assert.assertEquals(results.contains(c4), true, "Result does not contain all rectords 4");
     }
 }
